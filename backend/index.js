d4d8d2654d6fdfcd72245ae5382f51fd21b5c713
aquafinatrippy <taneldeviis@hotmail.com>
2021-03-24 00:33:54 +0200

    created extra endpoints for tickets: delete, create

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const tickets = require("./routes/tickets");
 
-//middleware
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-//routes
+
+
 app.use("/api", tickets);
 
-//run server
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
--- backend/routes/tickets.js
+++ backend/routes/tickets.js
@@ -1,9 +1,38 @@
 const router = require("express").Router();
 const Ticket = require("../models/Ticket");
-//all users
+
 router.get("/tickets", async (req, res) => {
-    const users = await Ticket.find({});
-    res.send(users);
+  try {
+    const tickets = await Ticket.find({});
+    res.send(tickets);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.post("/ticket", async (req, res) => {
+  try {
+    const ticket = new Ticket({
+      title: req.body.title,
+      description: req.body.description,
+      email: req.body.email,
+      priority: req.body.priority,
+      tickerNr: req.body.tickerNr,
+    });
+    await ticket.save();
+    res.send(ticket);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.delete("/ticket/:id", async (req, res) => {
+  try {
+    const ticket = await Ticket.findByIdAndDelete(req.params.id);
+    res.send(ticket);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = router;
