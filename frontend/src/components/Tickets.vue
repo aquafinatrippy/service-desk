1c87dc1867569babcb8946b2cb1ba0c413840a30
aquafinatrippy <taneldeviis@hotmail.com>
2021-03-24 13:02:53 +0200

    Vuex delete action added

--- frontend/src/components/Tickets.vue
+++ frontend/src/components/Tickets.vue
@@ -1,6 +1,15 @@
 <template>
   <v-row>
-    <v-col sm="12" md="6" v-for="(ticket, index) in tickets" v-bind:key="index">
+    <div v-if="tickets.length === 0">
+      <h2>There are no tasks</h2>
+    </div>
+    <v-col
+      sm="12"
+      md="6"
+      v-for="(ticket, index) in tickets"
+      v-bind:key="index"
+      v-else
+    >
       <v-card class="mx-auto" max-width="344" outlined>
         <v-list-item three-line>
           <v-list-item-content>
@@ -10,7 +19,16 @@
             <v-list-item-title class="headline mb-1">
               {{ ticket.title }}
             </v-list-item-title>
-            <v-list-item-subtitle>{{ticket.description}}</v-list-item-subtitle>
+            <v-list-item-subtitle>{{
+              ticket.description
+            }}</v-list-item-subtitle>
+            <v-rating
+              v-model="ticket.priority"
+              background-color="indigo lighten-3"
+              color="indigo"
+              small
+              readonly
+            ></v-rating>
           </v-list-item-content>
         </v-list-item>
 
@@ -18,7 +36,13 @@
           <v-btn outlined rounded text color="primary">
             Edit ticket
           </v-btn>
-          <v-btn outlined rounded text color="error">
+          <v-btn
+            outlined
+            rounded
+            text
+            color="error"
+            @click="deleteTicket(ticket._id)"
+          >
             close ticket
           </v-btn>
         </v-card-actions>
@@ -28,7 +52,7 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "Tickets",
@@ -36,5 +60,8 @@ export default {
     this.$store.dispatch("getTickets");
   },
   computed: mapState(["tickets"]),
+  methods: {
+    ...mapActions(["deleteTicket"]),
+  },
 };
 </script>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -18,6 +18,11 @@ export default new Vuex.Store({
       const { data } = await axios.get("http://localhost:8000/api/tickets");
       commit("SET_TICKETS", data);
     },
+    async deleteTicket({ commit }, id) {
+      await axios.delete(`http://localhost:8000/api/ticket/${id}`);
+      const tickets = this.tickets.filter((o) => o._id !== id);
+      commit("SET_TICKETS", tickets);
+    },
   },
   modules: {},
   getters: {
